4697d57726b1691053cfc564fcbe58c6215ba511
Vlad Faust <hey@vladfaust.com>
2023-02-14 18:20:07 +0400

    dev: add pug

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,13 @@
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template lang="pug">
+div
+  a(href="https://vitejs.dev" target="_blank")
+    img.logo(src="/vite.svg" alt="Vite logo")
+  a(href="https://vuejs.org/" target="_blank")
+    img.logo.vue(src="./assets/vue.svg" alt="Vue logo")
+HelloWorld(msg="Vite + Vue")
 </template>
 
 <style scoped>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,29 +6,23 @@ defineProps<{ msg: string }>();
 const count = ref(0);
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+<template lang="pug">
+h1 {{ msg }}
+.card
+  button(type="button" @click="count++")
+    | count is {{ count }}
+  p Edit
+    code components/HelloWorld.vue
+    | to test HMR
+p Check out
+  a(href="https://vuejs.org/guide/quick-start.html#local" target="_blank")
+    | create-vue
+  | , the official Vue + Vite starter
+p Install
+  a(href="https://github.com/johnsoncodehk/volar" target="_blank")
+    | Volar
+  | in your IDE for a better DX
+p.read-the-docs Click on the Vite and Vue logos to learn more
 </template>
 
 <style scoped>
